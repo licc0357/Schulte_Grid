acf7e863a638064850a69fd81619c02061a62580
瑞昌 李 <li845572249@outlook.com>
2023-08-28 09:37:26 +0800

    主界面

--- mfcs/Resource.h
+++ mfcs/Resource.h
@@ -1,21 +1,48 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
-// 由 mfcs.rc 使用
+// 供 mfcs.rc 使用
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_MFCS_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCS_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_BUTTON10                    1009
+#define IDC_BUTTON11                    1010
+#define IDC_BUTTON12                    1011
+#define IDC_BUTTON13                    1012
+#define IDC_BUTTON14                    1013
+#define IDC_BUTTON15                    1014
+#define IDC_BUTTON16                    1015
+#define IDC_BUTTON17                    1016
+#define IDC_BUTTON18                    1017
+#define IDC_BUTTON19                    1018
+#define IDC_BUTTON20                    1019
+#define IDC_BUTTON21                    1020
+#define IDC_BUTTON22                    1021
+#define IDC_BUTTON23                    1022
+#define IDC_BUTTON24                    1023
+#define IDC_BUTTON25                    1024
+#define IDC_BUTTON26                    1025
+#define IDC_START                       1025
 
-// 新对象的下一组默认值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- mfcs/mfcsDlg.h
+++ mfcs/mfcsDlg.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-
+#include<time.h>
 // CmfcsDlg 对话框
 class CmfcsDlg : public CDialogEx
 {
@@ -31,4 +31,19 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+	virtual void OnOK();
+public:
+	CMenu m_Menu;
+	// 按钮不可用
+	void dis_bn();
+	// 按钮随机初始化
+	void init_bn();
+	afx_msg void OnBnClickedStart();
+	// 按下数字
+	void on_num();
+	int num;
+	CTime t0;//开始时间年月日时分秒
+	time_t start, end;
+	// 总时间
+	double times;
 };
